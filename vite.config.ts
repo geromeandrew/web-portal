71f2dd0f0b3ad0ed59347477b713cc63a6c35bea
Gerome <geromeandrew7@gmail.com>
2026-09-09 17:18:57 +0800

    Configure local Docker development workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
-  const apiTarget = env.API_PROXY_TARGET?.replace(/\/$/, "") ?? "http://127.0.0.1:3001";
+  const apiTarget = (process.env.API_PROXY_TARGET ?? env.API_PROXY_TARGET ?? "http://127.0.0.1:3001").replace(/\/$/, "");
 
   return {
     plugins: [react()],
